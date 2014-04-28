127a401a114099de6d35af464f988370bb05175e
Luke Wen <wenluke427@gmail.com>
2014-04-28 14:09:39 +0100

    刚刚开始把workflow表达

---
+++ HEFT/src/cs/ncl/ac/uk/HEFT/algorithm.java
@@ -0,0 +1,18 @@
+package cs.ncl.ac.uk.HEFT;
+
+import java.util.HashMap;
+
+public class algorithm {
+	
+	public void ranking(){
+		// store the ranked value of node
+		HashMap<Integer,Integer> rank=new HashMap<Integer,Integer>();
+	}
+	
+	// the cost 
+	private int computeAva(int node){
+		int avCost = 0;
+		
+		return avCost;
+	}
+}
---
+++ HEFT/src/cs/ncl/ac/uk/test/workflow.java
@@ -0,0 +1,48 @@
+package cs.ncl.ac.uk.test;
+
+public class workflow {
+	// workflow the weight represents the data size 
+	int[][] workflow={{-1,10,5,-1,-1,-1,-1},
+					  {-1,-1,-1,15,-1,-1,-1},
+					  {-1,-1,-1,-1,14,-1,-1},
+					  {-1,-1,-1,-1,-1,200,-1},
+					  {-1,-1,-1,-1,-1,30,-1},
+					  {-1,-1,-1,-1,-1,-1,100},
+					  {-1,-1,-1,-1,-1,-1,-1}};
+	// communication cost
+	int [][] ccost ={{0,2,4},
+					 {3,0,1},
+					 {2,1,0}};
+	// CPU cost for each block on the clouds
+	int [][] cpucost={{30,20,16},
+			        {25,30,29},
+			        {10,35,26},
+			        {43,29,17},
+			        {20,45,12},
+			        {18,27,19},
+			        {47,65,61}};
+	// cloud security 
+	int [] cloud={0,1,1};
+	
+	// service security: clearance, location
+	int [][] ssecurity={{1,0},
+			            {0,0},
+			            {0,0},
+			            {1,1},
+			            {0,0},
+			            {1,0},
+			            {0,0}
+						};
+	
+	public int [][] getWorkflow(){
+		return workflow;
+	}
+	
+	public int[][] comCost(){
+		return ccost;
+	}
+	
+	public int[][] deployCost(){
+		return cpucost;
+	}
+}
